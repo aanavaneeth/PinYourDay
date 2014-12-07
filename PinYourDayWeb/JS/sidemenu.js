662980aee6e7923a547e92be3c300751e97c5c38
Navaneeth Agastya <agasthyanavaneeth@gmail.com>
2014-12-07 21:00:19 +0530

    Implemented "Add event" functionality
    But, Calendars are still using same DB

--- PinYourDayWeb/JS/sidemenu.js
+++ PinYourDayWeb/JS/sidemenu.js
@@ -5,23 +5,25 @@
 
     //Add events to menu items
     var buttonsList = document.querySelectorAll("core-item paper-button"),
+        appMenu = document.getElementById("appmenu"),
         buttons = Array.prototype.slice.call(buttonsList,0);
-    buttons.forEach(function(button){
-        button.addEventListener("click", function(evt){
-            document.getElementById("scaff").shadowRoot
-                .getElementsByTagName("core-drawer-panel")[0].setAttribute("selected", "main");
-            var id = evt.target.getAttribute("id");
-            Array.prototype.slice.call(document.querySelectorAll("core-scaffold div:not([data-menuassociation='"+ id+"'])"),0)
-                .forEach(function(node){
-                    node.style.display = "none";
-                });
-            document.querySelector("core-scaffold div[data-menuassociation='"+ id+"']").style.display = "block";
-        },false);
-    });
+    appMenu.addEventListener("click", function(evt){
+        console.log(evt.target.nodeName);
+            if((!!evt.target) && (evt.target.nodeName.toLowerCase() === "paper-button")){
+                document.getElementById("scaff").shadowRoot
+                            .getElementsByTagName("core-drawer-panel")[0].setAttribute("selected", "main");
+                        var id = evt.target.getAttribute("id");
+                console.log(id);
+                        Array.prototype.slice.call(document.querySelectorAll("#calendarPanel div:not([data-menuassociation='"+ id+"'])"),0)
+                            .forEach(function(node){
+                                node.style.display = "none";
+                            });
+                        document.querySelector("core-scaffold div[data-menuassociation='"+ id+"']").style.display = "block";
+            }
+    }, false);
 
     //display overlay for adding event
     var addCalendarButton = document.getElementById("addCalendar"),
-        appMenu = document.getElementById("appmenu"),
         overlay = document.getElementById("overlay"),
         addEventButton = document.getElementById("addEventButton"),
         eventvalue = document.getElementById("eventvalue");
@@ -31,21 +33,29 @@
 
     //Adding event
     addEventButton.addEventListener("click",function(){
+        var id;
         if(!!eventvalue.value){
-        appMenu.innerHTML += ("<core-item> <paper-button id='item2'  class='blue-ripple'>"+eventvalue.value +"</paper-button></core-item>");
+            localforage.getItem("MarkYourDayLocalDB")
+                .then(function(data){
+                    var localDb = data;
+                    if(!localDb){
+                        localDb = {};
+                        localDb.version = "0.0.1";
+                        localDb.events = [];
+                    }
+                    id = localDb.events.length+ 1;
+                    localDb.events.push({});
+                    return localforage.setItem("MarkYourDayLocalDB", localDb);
+                }, function(err){
+                }).then(function(){
+                    appMenu.innerHTML += ("<core-item> <paper-button id='"+ id +"'  class='blue-ripple'>"+eventvalue.value
+                    +"</paper-button></core-item>");
+                    document.querySelector("#calendarPanel").innerHTML += ("<div data-menuassociation = '"+ id
+                    +"' style='display:none;'><responsive-calendar></responsive-calendar></div>");
+                    eventvalue.value = "";
+                    overlay.close();
+                });
         }
-        eventvalue.value = "";
-        overlay.close();
-
-        //localforage.getItem("MarkYourDayLocalDB")
-        //    .then(function(data){
-        //        if(!data){
-        //            var data = {};
-        //            return localforage.setItem("MarkYourDayLocalDB", data);
-        //        }
-        //
-        //    }, function(err){
-        //    });
     }, false);
 
 
